3175da9e8fca532028e6e1c4d8c1af770471d3cc
majidmohammadian <majeedmohammadian@gmail.com>
2025-02-07 17:29:55 +0330

    added config file

---
+++ config/config.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Table Name
+    |--------------------------------------------------------------------------
+    |
+    | Table name in database
+    */
+
+    "tables" => [
+        'account' => 'accounts',
+        'bank' => 'banks',
+        'document' => 'documents',
+        'document_item' => 'document_items',
+        'document_automation' => 'document_automations',
+        'fiscal_year' => 'fiscal_years',
+        'document_duration' => 'document_durations',
+        'document_duration_status' => 'document_duration_statuses',
+        'stock' => 'stocks',
+    ],
+
+];
